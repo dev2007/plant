10979771e33118592a119cdbc521eb30b5ae4ad1
dev2007 <softengineerwu@gmail.com>
2025-11-26 08:38:19 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import Image from "next/image";
-import ThemeToggle from "./components/ThemeToggle";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Choice from "./components/Choice";
 import LanguageSelector from "./components/LanguageSelector";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function Home() {
   //国际化
   const translate = useTranslations();
 
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen flex flex-col">
       <nav className="w-full p-2 border border-base-200 flex justify-between">
         <div className="flex items-center space-x-1">
-          <Image src="/logo.png" alt="logo" width={30} height={30} />
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={30}
+            height={30}
+            className="animate-bounce"
+          />
           <h1 className="font-semibold">{translate("app")}</h1>
         </div>
         <div className="flex items-center">
@@ -21,6 +28,9 @@ export default function Home() {
           <LanguageSelector />
         </div>
       </nav>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <Choice />
+      </div>
     </div>
   );
 }
